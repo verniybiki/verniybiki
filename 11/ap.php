296087f7de3fb4a3cfa6162e83f2a4f6f9888440
verniybiki <53902929+verniybiki@users.noreply.github.com>
2019-08-08 20:05:17 +0800

    Add files via upload

---
+++ 11/ap.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html >
+
+<head>
+    <meta charset="UTF-8">
+<title>page1</title>
+</head>
+
+<body>
+    <?php
+   $Name=$_POST["UserName"];
+?>
+<p><i><?php echo $Name;?></i></P>
+   </body>
+
+</html>    
